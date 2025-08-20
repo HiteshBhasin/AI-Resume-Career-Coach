fe055d5bd7481b2752cf06aa4ecb4305563cc23e
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-08-20 00:10:15 +0000

    new commit

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,37 @@
+from __future__ import annotations
 from fastapi import FastAPI
 from fastapi import File, UploadFile
+from pydantic import BaseModel
+import asyncio
+from dotenv import load_dotenv
+from pypdf import PdfReader
 
+load_dotenv()
+
+
+
+app = FastAPI()
+
+@app.get("/")
+async def indexPage():
+    return {"message": "Welcome to my App"}
+    
+
+@app.post("/upload")
+async def processingFile(resume:UploadFile = File(...)):
+    contents = await resume.read()
+    resumer_text=extract_from_pdf(contents)
+    
+    
+    
+    
+    
+    
+def extract_from_pdf(pdf_path):
+    reader=PdfReader(pdf_path)
+    text=""
+    for pages in reader.pages:
+        text+=pages.extract_text()
+    return text
+        
+    
