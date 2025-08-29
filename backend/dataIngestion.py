093d4bdbf8aa2a7046352766822d89dc6ec4bd0d
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-08-29 14:23:15 +0000

    new commit

--- backend/dataIngestion.py
+++ backend/dataIngestion.py
@@ -1,8 +1,11 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
+from dotenv import load_dotenv
 
 def processingFile(text):
-    split_text = RecursiveCharacterTextSplitter
-
+    embedding = OpenAIEmbeddings(model ="text-embedding-3-large")
+    split_text = RecursiveCharacterTextSplitter(chunk_size =10 , chunk_overlap = 2)
+    text_for_agent = split_text.split_text(text)
+    
 
