6f787a23713a5a8756416db8b9a6fc425c72fe30
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-08-31 17:13:08 +0000

    new commit

--- backend/langGraphAlgo.py
+++ backend/langGraphAlgo.py
@@ -1,4 +1,9 @@
 from langgraph.graph import StateGraph
+from typing import TypedDict, Annotated, Any, Sequence
 from langgraph.graph import END, START, add_messages
 from langchain_core.messages import BaseMessage, ToolMessage
 from langchain_core.tools import tool
+
+class StateAgent(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    
