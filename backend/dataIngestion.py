824d9d05f20eb6110c3debcd822292ab809ed326
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-09-08 17:45:16 +0000

    New commit

--- backend/dataIngestion.py
+++ backend/dataIngestion.py
@@ -10,6 +10,7 @@ def processingFile(text):
     embedding = OpenAIEmbeddings(model ="text-embedding-3-large")
     split_text = RecursiveCharacterTextSplitter(chunk_size =250 , chunk_overlap = 0)
     text_for_agent = split_text.split_text(text)
+    # Chroma.from_documents()
     vector_store = Chroma.from_texts(text_for_agent, embedding, collection_name="resume_data")
     return vector_store
     
--- backend/langGraphAlgo.py
+++ backend/langGraphAlgo.py
@@ -1,12 +1,17 @@
 from langgraph.graph import StateGraph
 from typing import TypedDict, Annotated, Any, Sequence
 from langgraph.graph import END, START, add_messages
-from langchain_core.messages import BaseMessage, ToolMessage
+from langchain_core.messages import BaseMessage, ToolMessage, SystemMessage
 from langchain_core.tools import tool
 
 class StateAgent(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
-    
+
+system_prompt = SystemMessage(
+    content="You are an AI Career Coach. Review resumes and provide constructive, actionable feedback."
+)
+
 @tool
-def 
+def GiveFeedback():
+    
     
