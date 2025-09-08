655c7b6245ddcde2adef6a30f0ca8652fb58861b
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-09-08 12:46:46 +0000

    new commit

--- frontEnd/ai_resume_parser/src/component/resumePreview.js
+++ frontEnd/ai_resume_parser/src/component/resumePreview.js
@@ -8,5 +8,17 @@ if (!file){
 
 fileUrl = URL.createObjectURL(file);
 
-
+if (file.type==="application.pdf")
+    return (
+        <div>
+            <p>
+                <h1>Resume Preview</h1>
+            </p>
+            <iframe src="fileUrl" height="600 px" width="600 px" title="Resume Preview">
+                
+            </iframe>
+        </div>
+    );
+ 
 }
+export default FilePreview;
